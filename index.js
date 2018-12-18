4cbd29dc358b786d820430d774ee6bd47f1a719b
Michael Brattin <mbrattin@dmp.com>
2018-12-18 09:47:53 -0600

    for react testing

--- index.js
+++ index.js
@@ -2,17 +2,5 @@
 window.onload = loadPage;
 
 function loadPage() {
-  (function() {
-    var body = $('body');
-    var pageContent = $('.page-content');
-    var toggle = $('.menu-toggle');
-    var sideMenu = $('.menu-side');
-
-    $('.menu-toggle').bind('click', function() {
-      body.toggleClass('menu-open');
-      pageContent.toggleClass('menu-open');
-      sideMenu.toggleClass('add-shadow');
-      return false;
-    });
-  })();
+  
 }
